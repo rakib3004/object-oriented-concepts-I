9676af7c59bcfcc2d934a35d64c2ec6d3f9cbc4b
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-09-07 00:41:07 +0600

    Update and rename Constructor_with_input_Facilities to Constructor_with_input_Facilities.java

--- Constructor_with_input_Facilities.java
+++ Constructor_with_input_Facilities.java
@@ -1,11 +1,8 @@
 
 import java.util.Scanner;
-
 public class MainClass {
 
     public static void main (String[] args) {
-
-
         Scanner scan= new Scanner(System.in);
         int a1,b1,c1,d1;
         double p1,q1,r1,s1;
@@ -27,8 +24,6 @@ public class MainClass {
                a1 = scan.nextInt();
                b1 =scan.nextInt();
                 CalculationSystem cal1 = new CalculationSystem(a1,b1);
-
-
             }
             else if(ab==3)
             {
@@ -45,11 +40,7 @@ public class MainClass {
                 c1 = scan.nextInt();
                 d1 =scan.nextInt();
                 CalculationSystem cal3 = new CalculationSystem(a1,b1,c1,d1);
-
             }
-
-
-
         }
         if(option==2)
         {
@@ -93,92 +84,54 @@ class CalculationSystem {
     double p,q,r,s;
     int result_int;
     double result_double;
-
-
     CalculationSystem(int m,int n)
-    {
-        a=m;
+    {   a=m;
         b=n;
-
         result_int=a+b;
         System.out.println("Sum of two integer : " + result_int);
-
     }
-
-
     CalculationSystem(int m,int n,int x)
-    {
-
-        a=m;
+    {   a=m;
         b=n;
         c=x;
-
         result_int=a+b+c;
-        System.out.println("Sum of three integer : " + result_int);
-
-       
+        System.out.println("Sum of three integer : " + result_int);   
 
     }
-
-
     CalculationSystem(int m,int n,int x,int y)
     {
-
         a=m;
         b=n;
         c=x;
         d=y;
-
         result_int=a+b+c+d;
-        System.out.println("Sum of four integer : " + result_int );
-
-      
+        System.out.println("Sum of four integer : " + result_int );     
 
     }
-    CalculationSystem(double m,double n)
-    {
+    
+    CalculationSystem(double m,double n)    {
         p=m;
-
         q=n;
-
         result_double=p+q;
-        System.out.println("Sum of two double : " + result_double );
-
-        
-
+        System.out.println("Sum of two double : " + result_double );   
     }
-
 
     CalculationSystem(double m,double n,double x)
     {
-
         p=m;
         q=n;
         r=x;
-
         result_double=p+q+r;
-        System.out.println("Sum of three double : " + result_double);
-
-       
+        System.out.println("Sum of three double : " + result_double);       
     }
-
-
+    
     CalculationSystem(double m,double n,double x,double y)
     {
-
         p=m;
         q=n;
         r=x;
         s=y;
-
         result_double=p+q+r+s;
-        System.out.println("Sum of four double :" + result_double);
-
-       
-
+        System.out.println("Sum of four double :" + result_double);    
     }
-
-
-
-
-}
+        }
