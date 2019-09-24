c3b546335ed76268b49e333d6aee442b10205228
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-09-24 07:02:24 +0600

    Update Advance_Account_related_Class.java

--- Advance_Account_related_Class.java
+++ Advance_Account_related_Class.java
@@ -1,8 +1,210 @@
 package UserSection;
-import jdk.nashorn.internal.scripts.JO;
-
 import javax.swing.*;
 
+public class BankingSystem {
+    public static void main(String[] args) {
+
+        UserMenu pickpoint = new UserMenu();
+        BankMenu pickpoint2 = new BankMenu();
+     
+        pickpoint.Interaction();
+
+    }
+    public void developer_info(){
+        String curve ="Name : tarafder informatics Ltd.\nLocation :5/A,Nikunja,Dhaka-1229\nAuthor :Md .Rakib Trofder";
+        JOptionPane.showMessageDialog(null,curve);
+        JOptionPane.showMessageDialog(null, "Thanks to visit our site\nHope we will meet soon!!!!!");
+        JOptionPane.showMessageDialog(null, "Our mission \nto serve the nation");
+
+    }
+    public void recursive_menu(){
+        UserMenu pickpoint = new UserMenu();
+
+        pickpoint.Interaction();
+
+    }
+}
+
+
+package UserSection;
+import javax.swing.*;
+public class UserMenu {
+    public  void  Interaction() {
+
+        BankMenu creat_menu = new BankMenu();
+        Account show_info = new Account();
+        BankingSystem bank =new BankingSystem();
+        double amount;
+        String string_one,string_two,string_three;
+        int key_one;
+        int key_two;
+        String key_one_string;
+        JOptionPane.showMessageDialog(null, "Welcome to Agrani Bank Ltd.");
+
+        String menu1 = "1.Creat Account\n2.Show Infomation\n3.Show Balance\n4.Debit & Credit System\n5.Show Author Information";
+        key_one_string = JOptionPane.showInputDialog(null, "What do You Want ????\n" + menu1);
+        key_one = Integer.parseInt(key_one_string);
+
+        if(key_one==1){
+            creat_menu.account_initialization();
+        }
+        else if(key_one==2){
+            creat_menu.account_info_show();
+        }
+        else if(key_one==3){
+            creat_menu.checkBalance_show();
+        }
+        else if(key_one==4){
+            key_one_string =   JOptionPane.showInputDialog(null,"What do you want???\n1.withdraw\n2.deposit");
+            key_two = Integer.parseInt(key_one_string);
+            if(key_two==1){
+                   creat_menu.withdraw_show();
+            }
+            else if(key_two==2){
+                creat_menu.deposit_show();
+            }
+        }
+else if(key_one==5){
+
+    bank.developer_info();
+        }
+
+    }
+}
+
+
+
+package UserSection;
+import javax.swing.*;
+public class BankMenu {
+    Account array_0f_account[] = new Account[15];
+    int count,flag=0,iterator=0;
+    double amount;
+    String input_from_user;
+    BankingSystem bank =new BankingSystem();
+    static int parameter = 6;
+    int inputAccountNumber;
+  //  public  void set_account(){
+   // }
+    public void account_initialization() {
+        array_0f_account[0] = new Account("Zidan", "ME_IUT", "110962", "@zidan", "Mechanical");
+        array_0f_account[1] = new Account("Wasif", "CSE_JU", "110975", "@wasif", "Computer Science");
+        array_0f_account[2] = new Account("Azim", "SWE_KU", "110964", "@azim", "Soil & Water");
+        array_0f_account[3] = new Account("Rakib", "IIT_DU", "110965", "@rakib", "Software");
+        array_0f_account[4] = new Account("Mahmud", "CE_BUET", "110966", "@mahmud", "Civil");
+        array_0f_account[5] = new Account("Alif", "CSE_KUET", "110968", "@alif", "Computer Science");
+        array_0f_account[6] = new Account("Tahmeed", "IIT_DU", "110969", "@tahmeed", "Software");
+        array_0f_account[7] = new Account("Shifat","CSE_KUET","110971","@shifat","Computer Science");
+
+        String name, address, phone, photo, user_NID;
+        name = JOptionPane.showInputDialog("User Name");
+        address = JOptionPane.showInputDialog("User Address");
+        phone = JOptionPane.showInputDialog("User Phone");
+        photo = JOptionPane.showInputDialog("User Photo");
+        user_NID = JOptionPane.showInputDialog("User user_NID");
+        parameter++;
+        array_0f_account[parameter] = new Account(name, address, phone, photo, user_NID);
+        String user_details_message = "Name :" + name +"\n"+
+                "Address :"+ address +"\n"+
+                "Phone Number : "+  phone +"\n"+
+                "NID number : "+ user_NID +"\n"+
+                "Holder Photo :"+  photo ;
+         JOptionPane.showMessageDialog(null, user_details_message );
+        JOptionPane.showMessageDialog(null,"Account created successfully");
+        bank.recursive_menu();
+    }
+
+    public void account_info_show() {
+        String get_roll_string = JOptionPane.showInputDialog("Find the account number : ");
+        inputAccountNumber = Integer.parseInt(get_roll_string);
+
+        for( count =0;array_0f_account[count]!=null;count++){
+            JOptionPane.showMessageDialog(null, array_0f_account[count].checkAccountNumber());
+            if(array_0f_account[count].checkAccountNumber()==inputAccountNumber){
+                iterator = count;
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1){
+
+            JOptionPane.showMessageDialog(null ,array_0f_account[iterator].checkAccountHolderDetails());
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"Account not found");
+        }
+        bank.recursive_menu();
+        }
+        public void deposit_show(){
+            String get_roll_string = JOptionPane.showInputDialog("Find the account number : ");
+            inputAccountNumber = Integer.parseInt(get_roll_string);
+
+            for( count =0;array_0f_account[count]!=null;count++){
+                JOptionPane.showMessageDialog(null, array_0f_account[count].checkAccountNumber());
+                if(array_0f_account[count].checkAccountNumber()==inputAccountNumber){
+                    iterator = count;
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==1){
+                input_from_user =   JOptionPane.showInputDialog("Enter Deposit money :");
+                amount = Double.parseDouble(input_from_user);
+               array_0f_account[iterator].deposit(amount);
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Account not found");
+            }
+            bank.recursive_menu();
+        }
+        public void  withdraw_show(){
+            String get_roll_string = JOptionPane.showInputDialog("Find the account number : ");
+            inputAccountNumber = Integer.parseInt(get_roll_string);
+
+            for( count =0;array_0f_account[count]!=null;count++){
+                JOptionPane.showMessageDialog(null, array_0f_account[count].checkAccountNumber());
+                if(array_0f_account[count].checkAccountNumber()==inputAccountNumber){
+                    iterator = count;
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==1){
+                input_from_user =   JOptionPane.showInputDialog("Enter Deposit money :");
+                amount = Double.parseDouble(input_from_user);
+                array_0f_account[iterator].withdraw(amount);
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Account not found");
+            }
+            bank.recursive_menu();
+        }
+
+ public void  checkBalance_show(){
+            String get_roll_string = JOptionPane.showInputDialog("Find the account number : ");
+            inputAccountNumber = Integer.parseInt(get_roll_string);
+
+            for( count =0;array_0f_account[count]!=null;count++){
+                JOptionPane.showMessageDialog(null, array_0f_account[count].checkAccountNumber());
+                if(array_0f_account[count].checkAccountNumber()==inputAccountNumber){
+                    iterator = count;
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==1){
+
+                array_0f_account[iterator].checkBalance();
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Account not found");
+            }
+            bank.recursive_menu();
+        }
+    }
+
+package UserSection;
+import javax.swing.*;
 public class Account {
     private int  accountHolderNumber ;
     private  String accountHolderName;
@@ -63,6 +265,7 @@ public class Account {
 
 
     public int   checkAccountNumber(){
+
         return  this.accountHolderNumber;
     }
 
@@ -99,152 +302,5 @@ public class Account {
 
 
         }
-    }
-}
-
-package UserSection;
-
-
-        import javax.swing.*;
-
-
-
-
-public class BankingSystem {
-    public static void main(String[] args) {
-
-   /*  BankMenu agrani_bank_DU = new BankMenu();
-     agrani_bank_DU.account_initialization();
-     agrani_bank_DU.account_info_show();*/
-
-        UserMenu pickpoint = new UserMenu();
-        pickpoint.Interaction();
-
-    }
-}
-package UserSection;
-
-        import javax.swing.*;
-
-public class BankMenu {
-    Account array_0f_account[] = new Account[15];
-    static int parameter = 7;
-
-    public void account_initialization() {
-
-
-        array_0f_account[0] = new Account("Zidan", "ME_IUT", "110962", "@zidan", "Mechanical");
-        array_0f_account[1] = new Account("Wasif", "CSE_JU", "110975", "@wasif", "Computer Science");
-        array_0f_account[2] = new Account("Azim", "SWE_KU", "110964", "@azim", "Soil & Water");
-        array_0f_account[3] = new Account("Rakib", "IIT_DU", "110965", "@rakib", "Software");
-        array_0f_account[4] = new Account("Mahmud", "CE_BUET", "110966", "@mahmud", "Civil");
-        array_0f_account[6] = new Account("Alif", "CSE_KUET", "110968", "@alif", "Computer Science");
-        array_0f_account[7] = new Account("Tahmeed", "IIT_DU", "110969", "@tahmeed", "Software");
-
-        /* array_0f_account[9] = new Account("Shifat","CSE_KUET","110971","@shifat","Computer Science");*/
-
-        String name, address, phone, photo, user_NID;
-
-        name = JOptionPane.showInputDialog("User Name");
-        address = JOptionPane.showInputDialog("User Address");
-        phone = JOptionPane.showInputDialog("User Phone");
-        photo = JOptionPane.showInputDialog("User Photo");
-        user_NID = JOptionPane.showInputDialog("User user_NID");
-        parameter++;
-        array_0f_account[parameter] = new Account(name, address, phone, photo, user_NID);
-        account_info_show();
-    }
-
-    public void account_info_show() {
-        int inputAccountNumber;
-
-        String get_roll_string = JOptionPane.showInputDialog("Find the account number : ");
-        inputAccountNumber = Integer.parseInt(get_roll_string);
-
-        int count,flag=0,iterator=0;
-
-        int amount;
-        String input_from_user;
-        for( count =0;array_0f_account[count]!=null;count++){
-            if(array_0f_account[count].checkAccountNumber()==inputAccountNumber){
-
-                iterator = count;
-                flag=1;
-                break;
-            }
-        }
-        if(flag==1){
-            JOptionPane.showMessageDialog(null,array_0f_account[iterator].checkAccountHolderDetails());
-            input_from_user = JOptionPane.showInputDialog("Enter Your amount for deposit :");
-            amount = Integer.parseInt(input_from_user);
-            array_0f_account[iterator].deposit(amount );
-            JOptionPane.showMessageDialog(null,"Your Balance is :"+array_0f_account[iterator].checkBalance());
-            input_from_user = JOptionPane.showInputDialog("Enter Your amount to withdraw :");
-            amount = Integer.parseInt(input_from_user);
-            array_0f_account[iterator].withdraw(amount);
-            JOptionPane.showMessageDialog(null,"Your Balance is :"+array_0f_account[iterator].checkBalance());
-
-        }
-        else{
-            JOptionPane.showMessageDialog(null,"Account not found");
-        }
-
-
-    }
-}
-
-package UserSection;
-
-
-        import javax.swing.*;
-
-public class UserMenu {
-
-    public  void  Interaction() {
-
-        BankMenu creat_menu = new BankMenu();
-        Account show_info = new Account();
-        BankingSystem bank =new BankingSystem();
-        double amount;
-        String string_one,string_two,string_three;
-        int key_one;
-        int key_two;
-        String key_one_string;
-        JOptionPane.showMessageDialog(null, "Welcome to Agrani Bank Ltd.");
-
-        String menu1 = "1.Creat Account\n2.Show Infomation\n3.Show Balance\n4.Debit & Credit System\n5.Show Author Information";
-        key_one_string = JOptionPane.showInputDialog(null, "What do You Want ????\n" + menu1);
-        key_one = Integer.parseInt(key_one_string);
-
-        if(key_one==1){
-            creat_menu.account_initialization();
-        }
-        else if(key_one==2){
-            creat_menu.account_info_show();
-
-        }
-        else if(key_one==3){
-            show_info.checkBalance();
-        }
-        else if(key_one==4){
-            key_one_string =   JOptionPane.showInputDialog(null,"What do you want???\n1.withdraw\n2.deposit");
-            if(key_one_string=="1"){
-                key_one_string= JOptionPane.showInputDialog("Enter Your amount to withdraw :");
-                amount = Double.parseDouble(key_one_string);
-
-                show_info.withdraw(amount);
-            }
-            else if(key_one_string=="2"){
-                key_one_string= JOptionPane.showInputDialog("Enter Your amount to deposit :");
-                amount = Double.parseDouble(key_one_string);
-
-                show_info.deposit(amount);
-
-            }
-
-
-        }
-
-
     }
 }
