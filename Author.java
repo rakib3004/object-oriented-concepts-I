1f07aa3e67c8fdc7cca3c86551678683d4864468
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-10-29 16:47:06 +0600

    Add files via upload

---
+++ Author.java
@@ -0,0 +1,34 @@
+package AccountSystemManagement;
+
+public class Author {
+
+
+    private String name;
+    private String email;
+    private char gender;
+
+    public  Author(String name , String email , char gender){
+
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public char getGender(){
+        return gender;
+    }
+    public String toString(){
+        return "Author Name : "+getName()+"\n"+"Author Email"+getEmail()+"\n"+"Author Gender :"+getGender();
+    }
+
+
+}
+
+
+
