846dae08c00336b5e7433808becc5f99fa29747b
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-11-26 20:53:15 +0600

    Update Occupation_Concepts.java

--- Occupation_Concepts.java
+++ Occupation_Concepts.java
@@ -459,4 +459,47 @@ public class BussinessMan extends Urban {
     }
 
 }
+// teacher class
+
+package Occupation;
+
+public class Teacher extends  Urban {
+
+    private  int experience;
+    private  String qualification;
+    private  double payment;
+    public Teacher(){
+
+
+    }
+
+    public  Teacher(String name, int age, char gender, String residentialType,
+                    String cityName, String locationName, String occupationType,
+                    int cityId,int experience , String  qualification , double payment){
+        super(name,age,gender,residentialType,cityName,locationName,occupationType,cityId);
+this.experience =experience;
+this.qualification =qualification;
+this.payment= payment;
+    }
+
+    public  int getExperience(){
+        return  experience;
+    }
+    public  void  setExperience(int experience){
+        this.experience = experience;
+    }
+    public  String getQualification(){
+        return  qualification;
+    }
+    public  double getPayment(){
+        return payment;
+    }
+    public  void  setPayment(double payment){
+        this.payment  =payment;
+    }
+
+
+}
+
+
 
