798cf5aa3e609c529fff3c83a685af2bc619fcfc
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2019-11-26 20:58:30 +0600

    Update Occupation_Concepts.java

--- Occupation_Concepts.java
+++ Occupation_Concepts.java
@@ -459,6 +459,60 @@ public class BussinessMan extends Urban {
     }
 
 }
+
+// doctor class
+package Occupation;
+
+public class Doctor extends  Urban {
+
+    private String hospitalName;
+    private int patientNumber;
+    private  int doctorID;
+    private  double payment;
+    public  Doctor(){
+
+
+    }
+    public  Doctor(String name, int age, char gender, String residentialType, String cityName,
+                   String locationName, String occupationType, int cityId,String hospitalName,
+                   int patientNumber,int doctorID, double payment){
+
+        super(name,age,gender,residentialType,cityName,locationName,occupationType,cityId);
+        this.hospitalName = hospitalName;
+        this.patientNumber =patientNumber;
+        this.doctorID = doctorID;
+        this.payment = payment;
+
+    }
+
+    public  String  getHospitalName(){
+        return  hospitalName;
+    }
+    public  void  setHospitalName(String hospitalName){
+        this.hospitalName = hospitalName;
+    }
+    public  int getPatientNumber(){
+        return patientNumber;
+    }
+    public void setPatientNumber(int patientNumber){
+        this.patientNumber = patientNumber;
+    }
+    public int getDoctorID(){
+        return  doctorID;
+    }
+    public  void  setDoctorID(int doctorID){
+        this.doctorID =doctorID;
+    }
+    public  double getPayment(){
+        return  payment;
+    }
+    public void setPayment(double payment){
+        this.payment = payment;
+    }
+}
+
+
+
 // teacher class
 
 package Occupation;
