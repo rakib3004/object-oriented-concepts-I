f9cc6a5d71b7c07598ed7e148f56a341735516d6
rakib3004 <bsse1129@iit.du.ac.bd>
2021-08-24 23:40:49 +0600

    add new Input.java

---
+++ src/AdditionSystem/Input.java
@@ -0,0 +1,14 @@
+package src.AdditionSystem;
+
+import java.util.Scanner;
+
+public class Input {
+
+    public static void main(String[] args) {
+        System.out.print("abc: ");
+
+        Scanner scan = new Scanner(System.in);
+        int option = scan.nextInt();
+    }
+
+}
